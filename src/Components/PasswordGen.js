c697b472f3f7117a577fd856f4ce58b7218c691f
Abhishekjoshi98 <98544171+Abhishekjoshi98@users.noreply.github.com>
2022-11-10 17:38:21 +0530

    Update PasswordGen.js

--- src/Components/PasswordGen.js
+++ src/Components/PasswordGen.js
@@ -1,116 +1,36 @@
-// import React,{useState} from "react";
-
-// function PasswordGen() {
-
-//   const[password, setPassword] = useState("");
-
-//     let p =0;
-//   const passwordGenerator = () => {
-//     if(p<=0){
-//       return;
-//     }
-//     const upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//     const lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
-//     const numbers = "0123456789";
-//     const symbols = "!@#$*";
-//     let final = upperCaseChar + lowerCaseChar + numbers + symbols;
-//     let pass = "";
-//     for (let i = 0; i < p; i++) {
-//       let char = Math.floor(Math.random() * final.length + 1);
-//       console.log(char);
-//       pass += final.charAt(char);
-//     }
-//     setPassword(pass);
-//     const upperCase = document.getElementById('uppercase');
-//     const lowerCase = document.getElementById('lowercase');
-//     const number = document.getElementById('number');
-//     const sym = document.getElementById('symbol');
-//     const hasUpperCase = upperCase.checked
-//     const hasLowerCase = lowerCase.checked
-//     const hasNumber = number.checked
-//     const hasSymbol = sym.checked
-//     console.log(hasUpperCase,hasLowerCase,hasNumber,hasSymbol)
-//   };
-
-//   return (
-//     <div className="password flex">
-//       <h1>Password Generator</h1>
-//       <div>
-//         <input className="input" type={"text"} disabled value={password} />
-//         <br />
-//         <div className="length">
-//           <p>Select password length</p>
-//           <select className="select" onChange={(e)=>{p=e.target.value}}>
-//             <option className="option" value={"0"}>0</option>
-//             <option className="option"  value={"1"}>1</option>
-//             <option className="option"  value={"2"}>2</option>
-//             <option className="option"  value={"3"}>3</option>
-//             <option className="option"  value={"4"}>4</option>
-//             <option className="option"  value={"5"}>5</option>
-//             <option className="option"  value={"6"}>6</option>
-//             <option className="option"  value={"7"}>7</option>
-//             <option className="option"  value={"8"}>8</option>
-//           </select>
-//         </div>
-//         <br />
-//         <div>
-//         <input type={"checkbox"} id="uppercase" />
-//         <label htmlFor="uppercase">Include uppercase letters</label>
-//         <br />
-//         <input type={"checkbox"} id="lowercase" />
-//         <label htmlFor="lowercase">Include lowercase letters</label>
-//         <br />
-//         <input type={"checkbox"} id="number" />
-//         <label htmlFor="number">Include numbers</label>
-//         <br />
-//         <input type={"checkbox"} id="symbol" />
-//         <label htmlFor="symbol">Include symbols</label>
-//         </div>
-//         <br />
-//         <button className="btn" onClick={passwordGenerator}>Generate Password</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default PasswordGen;
-
 import {useState} from "react";
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import logo from '../icon/copy.png'
 
 const PasswordGen = () => {
-const [output,setOutput ] = useState("");
+const [output,setOutput  ] = useState("null");
+        const [isUpperChecked,setUpperChecked]= useState(false);
+        const [isLowerChecked,setLowerChecked]= useState(false);
+        const [isNumberChecked,setNumberChecked]= useState(false);
+        const [isSymbolChecked,setSymbolChecked]= useState(false);
     
-    var lengthVar = 0;
-    let checkUpper =false
-    let checkLower =false
-    let checkmiddle = false
-    let checkLast =false
-
-
-
-  const passwordGenerate = () => {
+    let lengthVar = 0;
+     const passwordGenerate = () => {
   
     const upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
-    const numbers = "0123456789";
-    const symbols = "!@#$%&*";
+    const number = "0123456789";
+    const symbol = "!@#$%&*";
     let finalChar ="";
     let length = lengthVar;
     let password = "";
 
-    if (checkUpper === true){
+    if (isUpperChecked === true){
         finalChar=finalChar+upperCaseChar
     }
-    if (checkLower === true){
+    if (isLowerChecked === true){
         finalChar=finalChar+lowerCaseChar
     }
-    if (checkmiddle === true){
-        finalChar=finalChar+numbers
+    if (isNumberChecked=== true){
+        finalChar=finalChar+number
     }
-    if (checkLast === true){
-        finalChar=finalChar+symbols
+    if (isSymbolChecked === true){
+        finalChar=finalChar+symbol
     }
 
     for (let i = 0; i < length; i++) {
@@ -137,28 +57,29 @@ const [output,setOutput ] = useState("");
       <div className="passlength">
       <p>Select Password length</p>
       <select className="option"onChange={(e)=>{lengthVar=e.target.value}}>
-        <option value="0">0</option>
-        <option value="1">1</option>
+        
         <option value="2">2</option>
         <option value="3">3</option>
         <option value="4">4</option>
         <option value="5">5</option>
         <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
       </select>
       </div>
       <br />
       <div className="checkbox">
-      <input onClick={()=>{checkUpper=!checkUpper}} type="checkbox" id="UpperCase" />
+      <input onClick={()=>{setUpperChecked(!isUpperChecked)}} type="checkbox" id="UpperCase" />
       <label htmlFor="UpperCase">Upper Case</label>
       <br />
-      <input onClick={()=>{checkLower=!checkLower}} type="checkbox" id="LowerCase" />
+      <input onClick={()=>{setLowerChecked(!isLowerChecked)}} type="checkbox" id="LowerCase" />
       <label htmlFor="LowerCase">Lower Case</label>
       <br />
-      <input onClick={()=>{checkmiddle=!checkmiddle}} type="checkbox" id="IncludeNumbers" />
-      <label htmlFor="IncludeNumbers">Include Numbers</label>
+      <input onClick={()=>{setNumberChecked(!isNumberChecked)}} type="checkbox" id="IncludeNumbers" />
+      <label htmlFor="IncludeNumbers">Include Number</label>
       <br />
-      <input onClick={()=>{checkLast=!checkLast}} type="checkbox" id="IncludeSymbols" />
-      <label htmlFor="IncludeSymbols">Include Symbols</label>
+      <input onClick={()=>{setSymbolChecked(!isSymbolChecked)}} type="checkbox" id="IncludeSymbols" />
+      <label htmlFor="IncludeSymbols">Include Symbol</label>
       </div >
       
       <button className="btngen" onClick={passwordGenerate}>Generate Password</button>
